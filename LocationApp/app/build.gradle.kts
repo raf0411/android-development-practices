130268bbc83027d2706fe5044cff006c1648a2de
Raffi <raffidarmawan0411@gmail.com>
2025-03-18 16:46:36 +0700

    finish LocationApp tutorial

--- LocationApp/app/build.gradle.kts
+++ LocationApp/app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- LocationApp/app/src/main/java/kalbe/corp/locationapp/MainActivity.kt
+++ LocationApp/app/src/main/java/kalbe/corp/locationapp/MainActivity.kt
@@ -3,6 +3,7 @@ package kalbe.corp.locationapp
 import android.Manifest.permission
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -11,7 +12,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,7 +22,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kalbe.corp.locationapp.ui.theme.LocationAppTheme
 import kalbe.corp.locationapp.utils.LocationUtils
 
@@ -28,30 +33,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel: LocationViewModel = viewModel()
+
             LocationAppTheme {
-                MyApp()
+                MyApp(viewModel)
             }
         }
     }
 }
 
 @Composable
-fun MyApp(){
-    val context = LocalContext.current
+fun MyApp(viewModel: LocationViewModel) {
+    val context: Context = LocalContext.current
     val locationUtils = LocationUtils(context)
-    LocationDisplay(locationUtils = locationUtils, context = context)
+
+    LocationDisplay(
+        locationUtils = locationUtils,
+        context = context,
+        locationViewModel = viewModel,
+    )
 }
 
 @Composable
 fun LocationDisplay(
     locationUtils: LocationUtils,
     context: Context,
+    locationViewModel: LocationViewModel,
 ) {
+    val location = locationViewModel.location.value
+    val address = location?.let {
+        locationUtils.reverseGeocodeLocation(location)
+    }
+
     val reqPermissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(), onResult = { permissions ->
-            if (permissions[permission.ACCESS_COARSE_LOCATION] == true && permissions[permission.ACCESS_FINE_LOCATION] == true) {
+            if (permissions[permission.ACCESS_FINE_LOCATION] == true || permissions[permission.ACCESS_COARSE_LOCATION] == true) {
                 // LOCATION ACCESS GRANTED
-
+                locationUtils.requestLocationUpdates(locationViewModel)
             } else {
                 // ASK FOR PERMISSION
                 val rationaleRequired = ActivityCompat.shouldShowRequestPermissionRationale(
@@ -79,17 +97,26 @@ fun LocationDisplay(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Text(text = "Location not available!")
+        if (location != null) {
+            Log.d("LocationValue", location.toString())
+            Text("Address: ${location.latitude} ${location.longitude} \n $address")
+        } else {
+            Text(text = "Location not available!")
+        }
+
+        Spacer(Modifier.height(16.dp))
+
         Button(
             onClick = {
+                Log.d("LocationValue", location.toString())
+
                 if (locationUtils.hasLocationPermission(context)) {
                     // Permission already granted update the location
-
+                    locationUtils.requestLocationUpdates(viewModel = locationViewModel)
                 } else {
                     reqPermissionLauncher.launch(
                         arrayOf(
-                            permission.ACCESS_COARSE_LOCATION,
-                            permission.ACCESS_FINE_LOCATION,
+                            permission.ACCESS_COARSE_LOCATION, permission.ACCESS_FINE_LOCATION
                         )
                     )
                 }
